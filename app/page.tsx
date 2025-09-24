956c405fa64f32721b760bf5a4bb015a166f207d
Jonathan Polochak <polochakjonathan@gmail.com>
2025-09-24 17:31:38 -0400

    new annimations and links

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ import 'aos/dist/aos.css';
 // 4️ Other libraries
 import AOS from 'aos';
 import { faInstagram, faCss3Alt, faSquareLinkedin, faHtml5, faSquareJs, faPhp, faPython, faGithub, faLinux, faReact } from '@fortawesome/free-brands-svg-icons';
-import { faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { faDatabase, faEnvelope, faPhone} from '@fortawesome/free-solid-svg-icons';
 import { faFileExcel, faFilePowerpoint } from '@fortawesome/free-regular-svg-icons';
 
 // 5️⃣ Components
@@ -59,17 +59,17 @@ export default function Home() {
     <div className={styles.page} id="home">
       <main className={styles.main}>
          {/* ------------------------------------------------------------ Nav Section ------------------------------------------------------------ */}
-        <div className={styles.nav} data-aos="fade-right" data-aos-duration="2000">
+        <div className={styles.nav} data-aos="fade-right" data-aos-duration="1000">
           <div className={styles.logo}>
             <img src="/assets/JpLogo.png"></img>
           </div>
           <div className={styles.links}>
-            <a href="#home">Home</a>
-            <a href="#aboutMe">About Me</a>
-            <a href="#skills">Skills</a>
-            <a href="#projects">Projects</a>
+            <a href="#home" data-aos="fade-right" data-aos-duration="1500" data-aos-delay="500">Home</a>
+            <a href="#aboutMe" data-aos="fade-right" data-aos-duration="1500" data-aos-delay="600">About Me</a>
+            <a href="#skills" data-aos="fade-right" data-aos-duration="1500" data-aos-delay="700">Skills</a>
+            <a href="#projects" data-aos="fade-right" data-aos-duration="1500" data-aos-delay="800">Projects</a>
           </div>
-          <div className={styles.contact}>
+          <div className={styles.contact} data-aos="fade-right" data-aos-duration="1500" data-aos-delay="900">
             <button className={styles.contactMeButton} onClick={() =>setIsContactOpen(true)}>Contact Me</button>
           </div>
         </div>
@@ -78,7 +78,7 @@ export default function Home() {
           <div className={styles.leftSection}>
            {/* ------------------------------------------------------------ Text Next to Center Circle Section ------------------------------------------------------------ */}
             <div className={styles.myText}>
-            <div data-aos="fade-right" data-aos-duration="2000" data-aos-delay="2000">
+            <div data-aos="fade-right" data-aos-duration="2000" data-aos-delay="1500">
               <div className={styles.greetingWrapper}>
                 <p><span className={styles.greeting}> Hello, my name is</span></p> <span className={styles.line}></span>
               </div>
@@ -86,7 +86,7 @@ export default function Home() {
               <h1>Jonathan Polochak</h1>
               <p>Majoring in <span className={styles.teal}>Information Technology</span></p>
               <TextType 
-                text={["I am a student at Coastal Carolina", "I am in my junior year"]}
+                text={["I'm a student at Coastal Carolina University", "I am in my junior year."]}
                 typingSpeed={75}
                 pauseDuration={2500}
                 showCursor={true}
@@ -95,8 +95,9 @@ export default function Home() {
               <div className={styles.socials}>
                 <p>My Socials</p>
                 <div className={styles.icons}>
-                  <FontAwesomeIcon icon={faInstagram} />
-                  <FontAwesomeIcon icon={faSquareLinkedin} />
+                  <a href="https://www.instagram.com/jonathanpolochak/"><FontAwesomeIcon icon={faInstagram} /></a>
+                  <a  href="https://www.linkedin.com/in/jonathan-polochak-a84a6b386/"><FontAwesomeIcon icon={faSquareLinkedin} /></a>
+                  <a  href="https://github.com/JPolochak99"><FontAwesomeIcon icon={faGithub} /></a>
                 </div>
               </div> 
             </div>
@@ -105,11 +106,11 @@ export default function Home() {
 
 
           {/* ------------------------------------------------------------ Center Circle & Picture Section ------------------------------------------------------------ */}  
-        <div className={styles.middleSection} data-aos="zoom-in" data-aos-duration="2000"  data-aos-delay="2000">
+        <div className={styles.middleSection} data-aos="zoom-in" data-aos-duration="2000"  data-aos-delay="1500">
         <div className={styles.mainPictureWrapper}>
           
           
-          <img src="/assets/blackTieCircle.png" alt="Me" />
+          <img src="/assets/blackTieCircle2.png" alt="Me" />
         </div>
         
         </div>
@@ -124,13 +125,13 @@ export default function Home() {
          {/* ------------------------------------------------------------ About Me Section ------------------------------------------------------------ */}
         <section id="aboutMe">
           <div className={styles.aboutMeContainer} >
-            <div className={styles.aboutMeText}> 
+            <div className={styles.aboutMeText} data-aos="fade-right"> 
               <h1>About Me</h1>
-              <p>My name is Jonathan Polochak, I am studying computer science at the University of Coastal Carolina. My short term goal is to find a summer 2026 intership in computer science. My long term goal is to become a front end developer. I enjoy combining creativity with problem-solving, and front-end development allows me to bring ideas to life visually while also focusing on user experience. In my free time, I work on personal projects such as building responsive websites and designing digital graphics to sharpen my skills. I am excited to continue learning and building real-world projects that will prepare me for my future career.</p>
+              <p>My name is Jonathan Polochak, I am studying computer science at the Coastal Carolina University. My short term goal is to find a summer 2026 intership in the Computer Science field. My long term goal is to become a front end developer. I enjoy combining creativity with problem-solving, and front-end development allows me to bring ideas to life visually while also focusing on user experience. In my free time, I work on personal projects such as building responsive websites and designing digital graphics to sharpen my skills. I am excited to continue learning and building real-world projects that will prepare me for my future career.</p>
             </div>
             <div className={styles.aboutMePictureContainer}>
               <div className={styles.aboutMePicture}>
-                <img src="../../assets/blueTie.png" data-aos="zoom-in"></img>
+                <img src="../../assets/blueTie.png"></img>
                 </div>
             </div>
           </div>
@@ -279,7 +280,7 @@ export default function Home() {
         
         </section>
     </div>
-    <hr className={styles.hr}></hr>
+    <hr className={styles.projectHr}></hr>
 
         {/* ------------------------------------------------------------ Project Section ------------------------------------------------------------ */}
         <section id="projects">
@@ -296,12 +297,14 @@ export default function Home() {
                 />
                 
                 <div className={styles.about}>
-                  <h1>About</h1>
+                  <div className={styles.aboutTitle}>
+                    <h2>About</h2>
+                  </div>
                   <p>Box Battles is a full-stack web application that allows users to create and participate in Super Bowl styled games for both the NFL and NBA. The platform features a real-time game management system, including dynamic team selection, automated score tracking, and live box score updates via the SportsData.io API for NFL games. Users can join or create a game, receive notifications via SMS integration and in-app notifications, and interact with a clean and responsive UI.</p>
                 </div>
 
                 <div className={styles.used}>
-                  <h1>Skills Used</h1>
+                  <h2>Skills Used</h2>
                   <ul>
                     <li>HTML & CSS</li>
                     <li>PHP</li>
@@ -341,12 +344,43 @@ export default function Home() {
         
 
 
-      <hr className={styles.hr}></hr>
+      <hr className={styles.projectHr}></hr>
       
 
 
-      <footer>
+      <footer className={styles.footer} >
+        <div className={styles.footerContainer} data-aos="fade-down">
+                  <div className={styles.footerImgContainer}>
+                    <img src="assets/JpLogo.png"></img>
+                  </div>
 
+                  <div className={styles.footerPageLinks}>
+                      <a href="#home">Home</a>
+                      <a href="#aboutMe">About Me</a>
+                      <a href="#skills">Skills</a>
+                      <a href="#projects">Projects</a>
+                  </div>
+
+                  <div className={styles.footerSocials}>
+                    <FontAwesomeIcon icon={faInstagram} />
+                    <FontAwesomeIcon icon={faSquareLinkedin} />
+                  </div>
+
+                  <div className={styles.footerEmail}>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    <p>polochakjonathan@gmail.com</p>
+                  </div>
+
+                  <div className={styles.footerPhone}>
+                    <FontAwesomeIcon icon={faPhone} />
+                    <p>631 951 8003</p>
+                  </div>
+
+                  <div className={styles.copyRight}>
+                    <hr className={styles.copyRightHr}></hr>
+                    <p> Copyright &copy; | Jonathan Polochak | 2025</p>
+                  </div>
+            </div>     
       </footer>
                   
 
